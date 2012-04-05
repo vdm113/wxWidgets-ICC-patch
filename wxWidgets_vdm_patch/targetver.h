3c878c910e1cd7ff4ad3ebc09e40d23080aebcf6
Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>
2012-04-05 14:14:51 +0200

    added ICL patch
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>
    
    Former-commit-id: 57bc9a266a66b6d48c32b5bba41c30d11fd6127b

---
+++ wxWidgets_vdm_patch/targetver.h
@@ -0,0 +1,15 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
+#pragma once
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
