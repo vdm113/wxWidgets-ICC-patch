5927ca995c52fa8c4ce14fb63a52f50dc8ea6d66
Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>
2012-04-05 14:14:51 +0200

    added ICL patch
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>

---
+++ wxWidgets_vdm_patch/stdafx.cpp
@@ -0,0 +1,15 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
+// stdafx.cpp : source file that includes just the standard includes
+// wxWidgets_vdm_patch.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
