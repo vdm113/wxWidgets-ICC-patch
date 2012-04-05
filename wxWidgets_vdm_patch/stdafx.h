b04b1d4b7a6ffa65645f94e45b74655b8d6655fd
Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>
2012-04-05 14:14:51 +0200

    added ICL patch
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>

---
+++ wxWidgets_vdm_patch/stdafx.h
@@ -0,0 +1,22 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
