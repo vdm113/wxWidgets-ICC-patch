a3d721aae468d05114f8d017a54721d40e214059
Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>
2012-05-16 10:01:39 +0200

    fix double-patched file
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>

--- src/stc/scintilla/src/Document.cxx
+++ src/stc/scintilla/src/Document.cxx
@@ -483,9 +483,6 @@ int Document::MovePositionOutsideChar(int pos, int moveDir, bool checkLineEnd) {
 #if defined(__INTEL_COMPILER) && 0
 #   pragma ivdep
 #endif
-#if defined(__INTEL_COMPILER)
-#   pragma ivdep
-#endif
 			while (posCheck < pos) {
 				char mbstr[maxBytesInDBCSCharacter+1];
 				int i;
