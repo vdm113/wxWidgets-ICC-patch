a516df3fcee90a6ff47ce1e2753fbe35f2f4eb65
Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>
2012-05-17 21:45:40 +0200

    use my (VDM) config as default + do not ignore additional compile-time "setup.h"'s
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>

---
+++ lib/vc_amd64_dll/mswud/wx/msw/rcdefs.h
@@ -0,0 +1,51 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#else
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef _WX_RCDEFS_H
+#define _WX_RCDEFS_H
+
+
+#define WX_MSC_FULL_VER 160040219
+
+
+
+#define WX_CPU_AMD64
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
