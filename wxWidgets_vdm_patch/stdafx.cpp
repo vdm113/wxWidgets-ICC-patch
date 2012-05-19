22b398b4f081264424ceb0b660d0d9c82afe5ca4
Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>
2012-05-19 14:09:13 +0200

    added licensing informations and credits
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm@vdm.no-ip.org>

--- wxWidgets_vdm_patch/stdafx.cpp
+++ wxWidgets_vdm_patch/stdafx.cpp
@@ -5,6 +5,15 @@
 #   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
 #endif
 
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wxWidgets_vdm_patch/stdafx.cpp
+// Purpose:     patcher for ICL compiler
+// Author:      Marian 'VooDooMan' Meravy
+// RCS-ID:      $Id$
+// Copyright:   (c) Marian 'VooDooMan' Meravy
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
 // stdafx.cpp : source file that includes just the standard includes
 // wxWidgets_vdm_patch.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
