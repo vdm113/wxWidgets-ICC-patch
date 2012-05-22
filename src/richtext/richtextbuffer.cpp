74cbd5ee1babd4b20d85acbf6878952ce435802e
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-05-22 21:25:58 +0200

    upstream has changed files
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/richtext/richtextbuffer.cpp
+++ src/richtext/richtextbuffer.cpp
@@ -13518,6 +13518,9 @@ wxRichTextFieldType *wxRichTextBuffer::FindFieldType(const wxString& name)
 void wxRichTextBuffer::CleanUpFieldTypes()
 {
     wxRichTextFieldTypeHashMap::iterator it;
+#if defined(__INTEL_COMPILER)
+#   pragma ivdep
+#endif
     for( it = sm_fieldTypes.begin(); it != sm_fieldTypes.end(); ++it )
     {
         wxRichTextFieldType* fieldType = it->second;
