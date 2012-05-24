98f5e5f0e20f4c6e14f121b84e240ff5d63d7a7f
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-05-24 08:53:44 +0200

    fixed broken merge
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/common/emptydmy.cpp
+++ src/common/emptydmy.cpp
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
 // This file exists so that it can be compiled into an object so the linker
 // will have something to chew on so that builds don't break when a platform
 // lacks any objects in a particular multilib.
