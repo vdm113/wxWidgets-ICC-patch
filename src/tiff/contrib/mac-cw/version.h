e8d8b4062e5a7b678b8bbd2caeac27d560040e80
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-05-24 08:53:44 +0200

    fixed broken merge
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/tiff/contrib/mac-cw/version.h
+++ src/tiff/contrib/mac-cw/version.h
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
 #define VERSION \
 "LIBTIFF, Version 3.4beta028 \n"\
 "Copyright (c) 1988-1995 Sam Leffler\n"\
