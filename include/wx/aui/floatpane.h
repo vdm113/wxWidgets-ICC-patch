04c9ff23ce3809dd4320e0123558ef9f14599ac3
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-05-24 08:53:44 +0200

    fixed broken merge
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- include/wx/aui/floatpane.h
+++ include/wx/aui/floatpane.h
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
 ///////////////////////////////////////////////////////////////////////////////
 // Name:        wx/aui/floatpane.h
 // Purpose:     wxaui: wx advanced user interface - docking window manager
