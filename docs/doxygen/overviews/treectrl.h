9373c53bf17a187626aa0209ae2410a97d5d62d5
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-05-24 08:53:44 +0200

    fixed broken merge
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- docs/doxygen/overviews/treectrl.h
+++ docs/doxygen/overviews/treectrl.h
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
 /////////////////////////////////////////////////////////////////////////////
 // Name:        treectrl.h
 // Purpose:     topic overview
