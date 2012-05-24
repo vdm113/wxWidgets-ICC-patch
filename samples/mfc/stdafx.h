0e77937aed06a1aec2749b3b463ba5faa61d9852
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-05-24 08:53:44 +0200

    fixed broken merge
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- samples/mfc/stdafx.h
+++ samples/mfc/stdafx.h
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
 // normally this is defined by the IDE by "Using MFC" option but if it isn't
 // (e.g. because we use bakefile-generated projects), do it ourselves to ensure
 // that MFC libraries are linked in
