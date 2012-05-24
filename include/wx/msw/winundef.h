5611d1afffc0e21736cc9e5fef18b29befec6fbb
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-05-24 08:53:44 +0200

    fixed broken merge
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- include/wx/msw/winundef.h
+++ include/wx/msw/winundef.h
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
 /////////////////////////////////////////////////////////////////////////////
 // Name:        winundef.h
 // Purpose:     undefine the common symbols #define'd by <windows.h>
