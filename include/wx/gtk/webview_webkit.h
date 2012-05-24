66a0faae9fbd4428b85e90e93ace649fd8a78187
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-05-24 08:53:44 +0200

    fixed broken merge
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- include/wx/gtk/webview_webkit.h
+++ include/wx/gtk/webview_webkit.h
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
+#elif !defined(MY_MACRO_PRAGMA_IVDEP)
+#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
+#endif
+
 /////////////////////////////////////////////////////////////////////////////
 // Name:        include/gtk/wx/webview.h
 // Purpose:     GTK webkit backend for web view component
