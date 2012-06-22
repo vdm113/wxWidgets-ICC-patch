0036dcbf90e69249af9fe25667f77c455a0ae657
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-06-22 03:13:08 +0200

    ICC patched files
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/ribbon/art_msw.cpp
+++ src/ribbon/art_msw.cpp
@@ -487,12 +487,18 @@ wxRibbonArtProvider* wxRibbonMSWArtProvider::Clone() const
 void wxRibbonMSWArtProvider::CloneTo(wxRibbonMSWArtProvider* copy) const
 {
     int i;
+#if defined(__INTEL_COMPILER)
+#   pragma ivdep
+#endif
     for(i = 0; i < 4; ++i)
     {
         copy->m_gallery_up_bitmap[i] = m_gallery_up_bitmap[i];
         copy->m_gallery_down_bitmap[i] = m_gallery_down_bitmap[i];
         copy->m_gallery_extension_bitmap[i] = m_gallery_extension_bitmap[i];
     }
+#if defined(__INTEL_COMPILER)
+#   pragma ivdep
+#endif
     for(i = 0; i < 2; ++i)
     {
         copy->m_panel_extension_bitmap[i] = m_panel_extension_bitmap[i];
