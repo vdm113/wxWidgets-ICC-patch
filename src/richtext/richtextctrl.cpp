2688d8941645785d4323861228b7f176f84a44ef
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-07-18 18:40:24 +0200

    patched loops for ICC
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/richtext/richtextctrl.cpp
+++ src/richtext/richtextctrl.cpp
@@ -4353,6 +4353,9 @@ wxRichTextRange wxRichTextCtrl::FindRangeForList(long pos, bool& isNumberedList)
         if (initialNode)
         {
             wxRichTextObjectList::compatibility_iterator startNode = initialNode->GetPrevious();
+#if defined(__INTEL_COMPILER)
+#   pragma ivdep
+#endif
             while (startNode)
             {
                 wxRichTextParagraph* p = wxDynamicCast(startNode->GetData(), wxRichTextParagraph);
@@ -4369,6 +4372,9 @@ wxRichTextRange wxRichTextCtrl::FindRangeForList(long pos, bool& isNumberedList)
 
             // Search forward
             wxRichTextObjectList::compatibility_iterator endNode = initialNode->GetNext();
+#if defined(__INTEL_COMPILER)
+#   pragma ivdep
+#endif
             while (endNode)
             {
                 wxRichTextParagraph* p = wxDynamicCast(endNode->GetData(), wxRichTextParagraph);
