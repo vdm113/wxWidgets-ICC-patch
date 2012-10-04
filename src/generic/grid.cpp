3510e2857b92641327916752cfaa7896b098f68d
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-10-04 21:04:35 +0200

    patch for ICC
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>
    
    Former-commit-id: 16eb7071fbf048f5c567c007087a01ee18fe23f3

--- src/generic/grid.cpp
+++ src/generic/grid.cpp
@@ -2965,6 +2965,9 @@ bool wxGrid::Redimension( wxGridTableMessage& msg )
             {
                 //Shift the column IDs
                 int i;
+#if defined(__INTEL_COMPILER)
+#   pragma ivdep
+#endif
                 for ( i = 0; i < m_numCols - numCols; i++ )
                 {
                     if ( m_colAt[i] >= (int)pos )
@@ -3041,6 +3044,9 @@ bool wxGrid::Redimension( wxGridTableMessage& msg )
 
                 //Set the new columns' positions
                 int i;
+#if defined(__INTEL_COMPILER)
+#   pragma ivdep
+#endif
                 for ( i = oldNumCols; i < m_numCols; i++ )
                 {
                     m_colAt[i] = i;
@@ -4548,6 +4554,9 @@ bool wxGrid::DoEndDragResizeLine(const wxGridOperations& oper)
                 // it by increasing the refresh area to cover it entirely if a
                 // part of it is affected
                 const int lineEnd = doper.PosToLine(this, posLineEnd, true);
+#if defined(__INTEL_COMPILER)
+#   pragma ivdep
+#endif
                 for ( ; line < lineEnd; line++ )
                 {
                     int cellLines = oper.Select(
