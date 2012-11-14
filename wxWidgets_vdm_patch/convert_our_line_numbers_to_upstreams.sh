5f0be5fa4d0398cc7e0d9edb241325443b10314c
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2012-11-14 01:32:31 +0100

    fix correct order of commands in support script
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- wxWidgets_vdm_patch/convert_our_line_numbers_to_upstreams.sh
+++ wxWidgets_vdm_patch/convert_our_line_numbers_to_upstreams.sh
@@ -6,8 +6,6 @@ rm -f log_out.tmp
 
 ( cat log.txt | awk -f convert_our_line_numbers_to_upstreams.awk - ) >log_out.tmp
 
-unix2dos log_out.tmp
-
 rm -f log_out2.tmp
 
 sed -r 's/^[0-9]+>//' log_out.tmp >log_out2.tmp
@@ -17,5 +15,7 @@ rm -f log_out.tmp
 ( cat log_out2.tmp | sort | uniq ) >log_out.txt
 
 rm -f log_out2.tmp
+
+unix2dos log_out.txt
 
 #eof
