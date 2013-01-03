b89851afd5ddccbfcad1529a87fc6e60b472d1d5
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2013-01-03 12:08:17 +0100

    fixed bug in scripts
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>
    
    Former-commit-id: 04620c54b7ed5dedeaaca3528a599361c83c3149

--- wxWidgets_vdm_patch/remove_our_prologue.sh
+++ wxWidgets_vdm_patch/remove_our_prologue.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find ../ -type f -iname '*.h' -o -iname '*.hh' -o -iname '*.hpp' -o -iname '*.hxx' -o -iname '*.c' -o -iname '*.cc' -o -iname '*.cpp' -o -iname '*.cxx' | while read fn ; do echo "======= '${fn}' =======" ; awk -f remove_our_prologue.awk "${fn}" >"${fn},tmp" ; mv -f "${fn},tmp" "${fn}" ; done
+find ../ -type f -iname '*.h' -o -iname '*.hh' -o -iname '*.hpp' -o -iname '*.hxx' | while read fn ; do echo "======= '${fn}' =======" ; awk -f remove_our_prologue.awk "${fn}" >"${fn},tmp" ; mv -f "${fn},tmp" "${fn}" ; done
 
 #eof
