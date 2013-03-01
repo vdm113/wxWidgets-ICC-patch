668915cc780aba18a30f532d5db516296e7ddc68
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2013-03-01 05:09:14 +0100

    sync with upstream SVN r73585
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

---
+++ wxWidgets_vdm_patch/stdafx.cpp
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wxWidgets_vdm_patch/stdafx.cpp
+// Purpose:     patcher for ICL compiler
+// Author:      Marian 'VooDooMan' Meravy
+// RCS-ID:      $Id$
+// Copyright:   (c) Marian 'VooDooMan' Meravy
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+// stdafx.cpp : source file that includes just the standard includes
+// wxWidgets_vdm_patch.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
