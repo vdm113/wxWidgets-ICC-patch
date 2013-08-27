bf2077a5c1d03c8055b8fa3c792ae9bf1d68f524
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2013-08-28 01:20:06 +0200

    added files to automatically get the commit forward and backward respoectionally
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

---
+++ rebase-second.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+for i in `grep -r '^<<<<<<< ' . | awk 'BEGIN { FS=":"; } { print $1; }'` ; do echo "$i" ; awk 'BEGIN { ignore=0; } /^<<<<<<< / { ignore=1; next; } /^=======$/ { ignore=0; next; } /^>>>>>>> / { ignore=0; next; } { if(0==ignore) print $0; }' "$i" >"${i}.tmp"; mv -f "${i}.tmp" "$i"; done
