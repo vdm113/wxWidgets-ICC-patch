afa79f4ff3390784e9227495af08d92e10100828
Václav Slavík <vslavik@fastmail.fm>
2013-09-06 17:09:23 +0000

    wxMSW: return correct value from wxMessageDialog::GetReturnCode().
    wxMessageDialog's native code didn't call SetReturnCode() as it should.
    As a result, it didn't work with ShowWindowModal().
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74776 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/msgdlg.cpp
+++ src/msw/msgdlg.cpp
@@ -601,7 +601,9 @@ int wxMessageDialog::ShowMessageBox()
     // do show the dialog
     int msAns = MessageBox(hWnd, message.t_str(), m_caption.t_str(), msStyle);
 
-    return MSWTranslateReturnCode(msAns);
+    int ret = MSWTranslateReturnCode(msAns);
+    SetReturnCode(ret);
+    return ret;
 }
 
 int wxMessageDialog::ShowModal()
@@ -636,7 +638,9 @@ int wxMessageDialog::ShowModal()
             msAns = IDOK;
         }
 
-        return MSWTranslateReturnCode( msAns );
+        int ret = MSWTranslateReturnCode(msAns);
+        SetReturnCode(ret);
+        return ret;
     }
 #endif // wxHAS_MSW_TASKDIALOG
 
