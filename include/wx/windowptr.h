2b246ec30d8c2ddd6770f19149134590adab6741
Václav Slavík <vslavik@fastmail.fm>
2013-09-06 17:09:16 +0000

    Add wxWindowPtr smart pointer.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74774 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/windowptr.h
+++ include/wx/windowptr.h
@@ -1,10 +1,3 @@
-/* token_VDM_prologue */
-#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
-#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
-#elif !defined(MY_MACRO_PRAGMA_IVDEP)
-#   define MY_MACRO_PRAGMA_IVDEP /* nevermind */
-#endif
-
 /////////////////////////////////////////////////////////////////////////////
 // Name:        wx/windowptr.h
 // Purpose:     smart pointer for holding wxWindow instances
