d068c6ceacbdf9816b9af487c1f16782cfc4400b
Vadim Zeitlin <vadim@wxwidgets.org>
2013-09-15 00:14:56 +0000

    Fix flickering of wxStaticBox background in wxMSW.
    First of all, don't erase background in WM_ERASEBKGND at all if we erase it
    anyhow in WM_PAINT, this is totally useless and is what wxBG_STYLE_PAINT is for.
    
    Second, clip out not only the siblings of the static box but also its children
    when erasing the background to avoid painting over the controls created as the
    box children, which is the preferred way to create them now.
    
    Closes #15150.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74811 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/statbox.cpp
+++ src/msw/statbox.cpp
@@ -291,9 +291,6 @@ WXHRGN wxStaticBox::MSWGetRegionWithoutChildren()
     // Also notice that we must iterate over all windows, not just all
     // wxWindows, as there may be composite windows etc.
     HWND child;
-#if defined(__INTEL_COMPILER) // VDM auto patch
-#   pragma ivdep
-#endif
     for ( child = ::GetWindow(GetHwndOf(GetParent()), GW_CHILD);
           child;
           child = ::GetWindow(child, GW_HWNDNEXT) )
@@ -348,9 +345,6 @@ WXHRGN wxStaticBox::MSWGetRegionWithoutChildren()
 
     // Also iterate over all children of the static box, we need to clip them
     // out as well.
-#if defined(__INTEL_COMPILER) // VDM auto patch
-#   pragma ivdep
-#endif
     for ( child = ::GetWindow(GetHwnd(), GW_CHILD);
           child;
           child = ::GetWindow(child, GW_HWNDNEXT) )
