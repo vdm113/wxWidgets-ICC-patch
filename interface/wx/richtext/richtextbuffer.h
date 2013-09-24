db6c7731f75150c2e5804169f7c319536ed786b5
Julian Smart <julian@anthemion.co.uk>
2013-09-24 12:05:54 +0000

    Added wxRichTextAction::SetOldAndNewObjects for clarity when submitting a wxRICHTEXT_CHANGE_OBJECT command.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74860 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/richtext/richtextbuffer.h
+++ interface/wx/richtext/richtextbuffer.h
@@ -5891,6 +5891,11 @@ public:
     void MakeObject(wxRichTextObject* obj) { m_objectAddress.Create(m_buffer, obj); }
 
     /**
+        Sets the existing and new objects, for use with wxRICHTEXT_CHANGE_OBJECT.
+    */
+    void SetOldAndNewObjects(wxRichTextObject* oldObj, wxRichTextObject* newObj);
+
+    /**
         Calculate arrays for refresh optimization.
     */
     void CalculateRefreshOptimizations(wxArrayInt& optimizationLineCharPositions, wxArrayInt& optimizationLineYPositions);
