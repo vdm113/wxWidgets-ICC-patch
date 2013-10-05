33fcabad9d2a6d882c611488a820e0c52e707834
Vadim Zeitlin <vadim@wxwidgets.org>
2013-10-05 23:30:52 +0000

    Fix capturing non-ASCII output using wxExecute().
    Explicitly use wxConvLibc with wxTextInputStream to make sure we correctly
    decode non-ASCII data in the subprocess output.
    
    This is a hack, the real solution would be to make wxTextInputStream work
    properly with wxConvAuto.
    
    See #14720.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74946 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/utilscmn.cpp
+++ src/common/utilscmn.cpp
@@ -639,11 +639,7 @@ static bool ReadAll(wxInputStream *is, wxArrayString& output)
     // Notice that wxTextInputStream doesn't work correctly with wxConvAuto
     // currently, see #14720, so use the current locale conversion explicitly
     // under assumption that any external program should be using it too.
-    wxTextInputStream tis(*is, " \t"
-#if wxUSE_UNICODE
-                                    , wxConvLibc
-#endif
-                                                );
+    wxTextInputStream tis(*is, " \t", wxConvLibc);
 
 #if defined(__INTEL_COMPILER)
 #   pragma ivdep
