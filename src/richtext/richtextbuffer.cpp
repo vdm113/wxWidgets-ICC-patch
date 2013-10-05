5accec09a7ed2572a53f591d08b89946183d1b14
Julian Smart <julian@anthemion.co.uk>
2013-10-05 16:31:30 +0000

    wxRTC: fixed bug with columns with unspecified widths, and column spans, being sized too small
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@74943 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/richtext/richtextbuffer.cpp
+++ src/richtext/richtextbuffer.cpp
@@ -10680,10 +10680,7 @@ bool wxRichTextTable::Layout(wxDC& dc, wxRichTextDrawingContext& context, const 
         }
     }
 
-    // (2) Allocate initial column widths from absolute values and proportions
-#if defined(__INTEL_COMPILER) // VDM auto patch
-#   pragma ivdep
-#endif
+    // (2) Allocate initial column widths from minimum widths, absolute values and proportions
     for (i = 0; i < m_colCount; i++)
     {
         if (absoluteColWidths[i] > 0)
@@ -10694,6 +10691,8 @@ bool wxRichTextTable::Layout(wxDC& dc, wxRichTextDrawingContext& context, const 
         {
             colWidths[i] = percentageColWidths[i];
         }
+        else
+            colWidths[i] = maxUnspecifiedColumnWidths[i];
     }
 
     // (3) Process absolute or proportional widths of spanning columns,
@@ -10768,7 +10767,7 @@ bool wxRichTextTable::Layout(wxDC& dc, wxRichTextDrawingContext& context, const 
                             // then we simply stop constraining the columns; instead, we'll just fit the spanning
                             // cells to the columns later.
                             cellWidth = cell->GetMinSize().x;
-                            
+
                             if (cell->GetMaxSize().x > cellWidth)
                                 cellWidth = cell->GetMaxSize().x;
                         }
