edc95c5f3118d5b84af95136a5e1367225489d60
Julian Smart <julian@anthemion.co.uk>
2013-10-20 17:42:06 +0000

    Corrected wxRTC IsDefault() implementations
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75052 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/richtext/richtextbuffer.h
+++ interface/wx/richtext/richtextbuffer.h
@@ -801,6 +801,11 @@ public:
     bool IsValid() const { return HasWidth(); }
 
     /**
+        True if the border has no attributes set.
+    */
+    bool IsDefault() const { return (m_flags == 0); }
+
+    /**
         Set the valid flag for this border.
     */
     void MakeValid() { m_borderWidth.SetValid(true); }
@@ -913,7 +918,7 @@ public:
     void CollectCommonAttributes(const wxTextAttrBorders& attr, wxTextAttrBorders& clashingAttr, wxTextAttrBorders& absentAttr);
 
     /**
-        Returns @true if all borders are valid.
+        Returns @true if at least one border is valid.
     */
     bool IsValid() const { return m_left.IsValid() || m_right.IsValid() || m_top.IsValid() || m_bottom.IsValid(); }
 
