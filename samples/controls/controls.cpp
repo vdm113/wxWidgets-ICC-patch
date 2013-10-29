f39e58ca5e01c0b883e394af3adc7745f1980e15
Vadim Zeitlin <vadim@wxwidgets.org>
2013-10-29 19:20:15 +0000

    Avoid pointer conversions to unsigned long in the samples.
    This results in errors when using g++ Win64 builds as sizeof(long) <
    sizeof(void*) in Win64.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_3_0_BRANCH@75100 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/controls/controls.cpp
+++ samples/controls/controls.cpp
@@ -2070,7 +2070,7 @@ void MyFrame::OnIdle( wxIdleEvent& WXUNUSED(event) )
 #endif
                     , s_windowFocus->GetName().c_str()
 #ifdef __WXMSW__
-                    , (unsigned int) s_windowFocus->GetHWND()
+                    , (unsigned)wxPtrToUInt(s_windowFocus->GetHWND())
 #endif
                       );
         }
