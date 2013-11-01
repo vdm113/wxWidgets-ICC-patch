34a687a6fdcddc320b772cbc13b9e5d0776ec8f5
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2013-11-01 11:48:01 +0100

    remove patched for ICC prologue
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- wxWidgets_vdm_patch/wxWidgets_vdm_patch.cpp
+++ wxWidgets_vdm_patch/wxWidgets_vdm_patch.cpp
@@ -1,10 +1,3 @@
-/* token_VDM_prologue */
-#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(MY_MACRO_PRAGMA_IVDEP)
-#   define MY_MACRO_PRAGMA_IVDEP __pragma(ivdep)
-#elif !defined(MY_MACRO_PRAGMA_IVDEP)
-#   define MY_MACRO_PRAGMA_IVDEP
-#endif
-
 /////////////////////////////////////////////////////////////////////////////
 // Name:        wxWidgets_vdm_patch/stdafx.cpp
 // Purpose:     patcher for ICL compiler
