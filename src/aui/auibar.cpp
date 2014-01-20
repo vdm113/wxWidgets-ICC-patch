52cf4bdbd61b8daa34bc22fb6fd81022de3c3a83
Vadim Zeitlin <vadim@wxwidgets.org>
2014-01-20 14:45:23 +0000

    Don't pass input parameter by non-const reference unnecessarily.
    This is confusing.
    
    See #15893.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75659 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/aui/auibar.cpp
+++ src/aui/auibar.cpp
@@ -787,7 +787,7 @@ int wxAuiDefaultToolBarArt::ShowDropDown(wxWindow* wnd,
 
 
 
-static wxOrientation GetOrientation(long& style)
+static wxOrientation GetOrientation(long style)
 {
     switch (style & wxAUI_ORIENTATION_MASK)
     {
