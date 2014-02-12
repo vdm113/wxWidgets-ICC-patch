a59157cf451cd9dc0d78f52af38a632d3b2d997d
Vadim Zeitlin <vadim@wxwidgets.org>
2014-02-12 13:15:17 +0000

    Provide more detailed deprecation message for wxFont ctor.
    Be more clear about how should the code be changed.
    
    Closes #15972.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75875 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775
    
    Former-commit-id: dfd035518e2fe3bb2e854392ccb0df44cf98f2fe

--- include/wx/msw/font.h
+++ include/wx/msw/font.h
@@ -117,7 +117,7 @@ public:
 
     virtual bool IsFixedWidth() const;
 
-    wxDEPRECATED_MSG("use wxFONT{FAMILY,STYLE,WEIGHT}_XXX constants")
+    wxDEPRECATED_MSG("use wxFONT{FAMILY,STYLE,WEIGHT}_XXX constants ie: wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD")
     wxFont(int size,
            int family,
            int style,
@@ -129,7 +129,7 @@ public:
         (void)Create(size, (wxFontFamily)family, (wxFontStyle)style, (wxFontWeight)weight, underlined, face, encoding);
     }
 
-    wxDEPRECATED_MSG("use wxFONT{FAMILY,STYLE,WEIGHT}_XXX constants")
+    wxDEPRECATED_MSG("use wxFONT{FAMILY,STYLE,WEIGHT}_XXX constants ie: wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD")
     wxFont(const wxSize& pixelSize,
            int family,
            int style,
