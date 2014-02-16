396eb9891805290dd4e3493055b2f2fbbbe3da72
Julian Smart <julian@anthemion.co.uk>
2014-02-16 20:05:19 +0000

    Removed rounding that didn't work for negative numbers; added ability to hide "move object" controls
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75893 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/richtext/richtextsizepage.cpp
+++ src/richtext/richtextsizepage.cpp
@@ -72,6 +72,7 @@ bool wxRichTextSizePage::sm_showAlignmentControls = true;
 bool wxRichTextSizePage::sm_showFloatingAndAlignmentControls = true;
 bool wxRichTextSizePage::sm_enablePositionAndSizeUnits = true;
 bool wxRichTextSizePage::sm_enablePositionAndSizeCheckboxes = true;
+bool wxRichTextSizePage::sm_showMoveObjectControls = true;
 
 /*!
  * wxRichTextSizePage constructors
@@ -124,6 +125,7 @@ wxRichTextSizePage::~wxRichTextSizePage()
     sm_showMinMaxSizeControls = true;
     sm_showMinMaxSizeControls = true;
     sm_enablePositionAndSizeCheckboxes = true;
+    sm_showMoveObjectControls = true;
     
 ////@begin wxRichTextSizePage destruction
 ////@end wxRichTextSizePage destruction
@@ -748,6 +750,9 @@ void wxRichTextSizePage::CreateControls()
 
     if (!sm_showPositionModeControls)
         m_moveObjectParentSizer->Show(m_positionModeSizer, false);
+
+    if (!sm_showMoveObjectControls)
+        m_moveObjectParentSizer->Show(m_moveObjectSizer, false);
 }
 
 wxRichTextAttr* wxRichTextSizePage::GetAttributes()
