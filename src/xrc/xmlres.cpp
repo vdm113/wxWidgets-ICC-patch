61201295d1aed17cbb732807a801a611142330c1
Vadim Zeitlin <vadim@wxwidgets.org>
2014-02-22 22:39:56 +0000

    Remove confusing delete from wxIdRangeManager dtor.
    The singleton dtor shouldn't delete the global singleton object, if this were
    ever really possible, it would result in an infinite recursion. And even
    though it was not (because ms_instance was reset to NULL before destroying the
    object pointed by it), this delete was still confusing and unnecessary, so
    remove it.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75988 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/xrc/xmlres.cpp
+++ src/xrc/xmlres.cpp
@@ -1335,8 +1335,6 @@ wxIdRangeManager::~wxIdRangeManager()
         delete *i;
     }
     m_IdRanges.clear();
-
-    delete ms_instance;
 }
 
 void wxIdRangeManager::AddRange(const wxXmlNode* node)
