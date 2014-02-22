17d8e6342ea3c00daf2ec8682bef94ba0182e416
Vadim Zeitlin <vadim@wxwidgets.org>
2014-02-22 22:40:00 +0000

    Improve parameter validation in wxStream code.
    Replace wxASSERT_MSG()s with wxCHECK_MSG()s to ensure that we don't crash even
    if we are passed a NULL pointer, saying "Warning: Null pointer is about to be
    used" and crashing just isn't very useful in the grand scheme of things.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75989 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/stream.cpp
+++ src/common/stream.cpp
@@ -397,7 +397,7 @@ char wxStreamBuffer::GetChar()
 
 size_t wxStreamBuffer::Read(void *buffer, size_t size)
 {
-    wxASSERT_MSG( buffer, wxT("Warning: Null pointer is about to be used") );
+    wxCHECK_MSG( buffer, 0, wxT("NULL data pointer") );
 
     /* Clear buffer first */
     memset(buffer, 0x00, size);
@@ -487,7 +487,7 @@ size_t wxStreamBuffer::Read(wxStreamBuffer *dbuf)
 
 size_t wxStreamBuffer::Write(const void *buffer, size_t size)
 {
-    wxASSERT_MSG( buffer, wxT("Warning: Null pointer is about to be send") );
+    wxCHECK_MSG( buffer, 0, wxT("NULL data pointer") );
 
     if (m_stream)
     {
@@ -796,7 +796,7 @@ char *wxInputStream::AllocSpaceWBack(size_t needed_size)
 
 size_t wxInputStream::GetWBack(void *buf, size_t size)
 {
-    wxASSERT_MSG( buf, wxT("Warning: Null pointer is about to be used") );
+    wxCHECK_MSG( buf, 0, wxT("NULL data pointer") );
 
     /* Clear buffer first */
     memset(buf, 0x00, size);
@@ -832,7 +832,7 @@ size_t wxInputStream::GetWBack(void *buf, size_t size)
 
 size_t wxInputStream::Ungetch(const void *buf, size_t bufsize)
 {
-    wxASSERT_MSG( buf, wxT("Warning: Null pointer is about to be used in Ungetch()") );
+    wxCHECK_MSG( buf, 0, wxT("NULL data pointer") );
 
     if ( m_lasterror != wxSTREAM_NO_ERROR && m_lasterror != wxSTREAM_EOF )
     {
@@ -866,7 +866,7 @@ int wxInputStream::GetC()
 
 wxInputStream& wxInputStream::Read(void *buf, size_t size)
 {
-    wxASSERT_MSG( buf, wxT("Warning: Null pointer is about to be read") );
+    wxCHECK_MSG( buf, *this, wxT("NULL data pointer") );
 
     char *p = (char *)buf;
     m_lastcount = 0;
