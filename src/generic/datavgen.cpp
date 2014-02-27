eb0fea10c3780285316ea3ce0ec87010464bfd58
Vadim Zeitlin <vadim@wxwidgets.org>
2014-02-27 15:46:32 +0000

    Fix Intel compiler warnings about hiding member variables.
    Rename method parameters to avoid conflicts with member variables names.
    
    Closes #15971.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76029 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/datavgen.cpp
+++ src/generic/datavgen.cpp
@@ -3189,10 +3189,10 @@ int wxDataViewMainWindow::GetLineHeight( unsigned int row ) const
 class RowToTreeNodeJob: public DoJob
 {
 public:
-    RowToTreeNodeJob( unsigned int row_ , int current, wxDataViewTreeNode * node )
+    RowToTreeNodeJob( unsigned int row_ , int current_, wxDataViewTreeNode * node )
     {
         this->row = row_;
-        this->current = current;
+        this->current = current_;
         ret = NULL;
         parent = node;
     }
