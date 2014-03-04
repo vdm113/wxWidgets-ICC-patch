9faa9ff151e7b3465310f296b1afda8f25b1fee0
Vadim Zeitlin <vadim@wxwidgets.org>
2014-03-04 14:07:43 +0000

    Compilation fix for wxUniv/MSW in wxTLW code dealing with menus.
    The code used by WM_INITMENUPOPUP and WM_EXITMENULOOP handlers shouldn't be
    used in wxUniv build as it's not used there anyhow and doesn't even compile.
    
    Closes #16039.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76077 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/toplevel.cpp
+++ src/msw/toplevel.cpp
@@ -1488,7 +1488,7 @@ void wxTopLevelWindowMSW::OnActivate(wxActivateEvent& event)
     }
 }
 
-#if wxUSE_MENUS
+#if wxUSE_MENUS && !defined(__WXUNIVERSAL__)
 
 bool
 wxTopLevelWindowMSW::HandleMenuSelect(WXWORD nItem, WXWORD flags, WXHMENU hMenu)
@@ -1595,7 +1595,7 @@ wxMenu* wxTopLevelWindowMSW::MSWFindMenuFromHMENU(WXHMENU WXUNUSED(hMenu))
     return NULL;
 }
 
-#endif // wxUSE_MENUS
+#endif // wxUSE_MENUS && !__WXUNIVERSAL__
 
 
 
