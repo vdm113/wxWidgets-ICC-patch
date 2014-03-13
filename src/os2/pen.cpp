75b0707172d41917d7246ec3dceecf618c2c8b65
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2014-03-13 06:44:38 +0100

    patched for ICC; sync with upstream; remove files we do not support
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/os2/pen.cpp
+++ src/os2/pen.cpp
@@ -155,7 +155,6 @@ wxPen::wxPen(
     RealizeResource();
 } // end of wxPen::wxPen
 
-#if WXWIN_COMPATIBILITY_3_0
 wxPen::wxPen(const wxColour& colour, int width, int style)
 {
     m_refData = new wxPenRefData(colour, width, (wxPenStyle)style);
