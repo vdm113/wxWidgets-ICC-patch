aa2321029707b7f6906f42cfa4c62b0adb4e7704
Stefan Csomor <csomor@advancedconcepts.ch>
2014-03-31 07:28:10 +0000

    removing extra semicola
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76231 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/osx/core/bitmap.cpp
+++ src/osx/core/bitmap.cpp
@@ -1901,7 +1901,7 @@ class WXDLLEXPORT wxBundleResourceHandler: public wxBitmapHandler
 public:
     inline wxBundleResourceHandler()
     {
-    };
+    }
     
     virtual bool LoadFile(wxBitmap *bitmap,
                           const wxString& name,
@@ -1922,7 +1922,7 @@ public:
         SetName(wxT("PNG resource"));
         SetExtension("PNG");
         SetType(wxBITMAP_TYPE_PNG_RESOURCE);
-    };
+    }
 };
 
 IMPLEMENT_DYNAMIC_CLASS(wxPNGResourceHandler, wxBundleResourceHandler)
@@ -1937,7 +1937,7 @@ public:
         SetName(wxT("JPEG resource"));
         SetExtension("JPEG");
         SetType(wxBITMAP_TYPE_JPEG_RESOURCE);
-    };
+    }
 };
 
 IMPLEMENT_DYNAMIC_CLASS(wxJPEGResourceHandler, wxBundleResourceHandler)
