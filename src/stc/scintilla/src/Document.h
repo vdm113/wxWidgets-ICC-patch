dad39658f98744a70cdd9c7761d439eed00d3502
Jouk Jansen <joukj@hrem.nano.tudelft.nl>
2014-04-01 08:56:05 +0000

    OpenVMS (Alpha only) uses != operator in std::find ->needs operator defenition
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76249 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/stc/scintilla/src/Document.h
+++ src/stc/scintilla/src/Document.h
@@ -201,6 +201,9 @@ public:
 		bool operator==(const WatcherWithUserData &other) const {
 			return (watcher == other.watcher) && (userData == other.userData);
 		}
+		bool operator!=(const WatcherWithUserData &other) const {
+			return (watcher != other.watcher) || (userData != other.userData);
+		}
 	};
 
 private:
