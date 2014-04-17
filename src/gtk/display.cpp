f1dfc7da259abac5c10902cb6aa517e39c20d0f8
Paul Cornett <paulcor@bullseye.com>
2014-04-17 17:36:10 +0000

    Add a separate GTK-specific wxDisplay implementation. This separates the GTK implementation from Unix/X11, except for the wxVideoMode stuff, and _NET_WORKAREA when GTK < 3.4. wxDisplay and wxClientDisplayRect() should now work as well as they can with Wayland.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76365 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/display.cpp
+++ src/gtk/display.cpp
@@ -1,10 +1,3 @@
-/* token_VDM_prologue */
-#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(VDM_MACRO_PRAGMA_IVDEP)
-#   define VDM_MACRO_PRAGMA_IVDEP __pragma(ivdep)
-#elif !defined(VDM_MACRO_PRAGMA_IVDEP)
-#   define VDM_MACRO_PRAGMA_IVDEP
-#endif
-
 ///////////////////////////////////////////////////////////////////////////
 // Name:        src/gtk/display.cpp
 // Author:      Paul Cornett
@@ -25,9 +18,8 @@
 #ifdef GDK_WINDOWING_X11
     #include <gdk/gdkx.h>
 #endif
-#include "wx/gtk/private/gtk2-compat.h"
 
-GdkWindow* wxGetTopLevelGDK();
+GtkWidget* wxGetRootWindow();
 
 //-----------------------------------------------------------------------------
 
@@ -63,12 +55,11 @@ wx_gdk_screen_get_monitor_workarea(GdkScreen* screen, int monitor, GdkRectangle*
         if (GDK_IS_X11_SCREEN(screen))
 #endif
         {
-            GdkRectangle rect = { 0 };
+            GdkRectangle rect;
             wxGetWorkAreaX11(GDK_SCREEN_XSCREEN(screen),
                 rect.x, rect.y, rect.width, rect.height);
             // in case _NET_WORKAREA result is too large
-            if (rect.width && rect.height)
-                gdk_rectangle_intersect(dest, &rect, dest);
+            gdk_rectangle_intersect(dest, &rect, dest);
         }
 #endif // GDK_WINDOWING_X11
     }
@@ -83,7 +74,7 @@ void wxClientDisplayRect(int* x, int* y, int* width, int* height)
     GdkRectangle rect = { 0, 0, 672, 396 };
 #else
     GdkRectangle rect;
-    GdkWindow* window = wxGetTopLevelGDK();
+    GdkWindow* window = gtk_widget_get_window(wxGetRootWindow());
     GdkScreen* screen = gdk_window_get_screen(window);
     int monitor = gdk_screen_get_monitor_at_window(screen, window);
     gdk_screen_get_monitor_workarea(screen, monitor, &rect);
@@ -126,7 +117,7 @@ public:
 
 static inline GdkScreen* GetScreen()
 {
-    return gdk_window_get_screen(wxGetTopLevelGDK());
+    return gtk_widget_get_screen(wxGetRootWindow());
 }
 //-----------------------------------------------------------------------------
 
