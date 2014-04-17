d6f646d678d6f5705a456fd467d43d96d5805cc1
Paul Cornett <paulcor@bullseye.com>
2014-04-17 17:36:10 +0000

    Add a separate GTK-specific wxDisplay implementation. This separates the GTK implementation from Unix/X11, except for the wxVideoMode stuff, and _NET_WORKAREA when GTK < 3.4. wxDisplay and wxClientDisplayRect() should now work as well as they can with Wayland.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76365 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/gtk/display.cpp
+++ src/gtk/display.cpp
@@ -1,10 +1,3 @@
-/* token_VDM_prologue */
-#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(VDM_MACRO_PRAGMA_IVDEP)
-#   define VDM_MACRO_PRAGMA_IVDEP __pragma(ivdep)
-#elif !defined(VDM_MACRO_PRAGMA_IVDEP)
-#   define VDM_MACRO_PRAGMA_IVDEP
-#endif
-
 ///////////////////////////////////////////////////////////////////////////
 // Name:        src/gtk/display.cpp
 // Author:      Paul Cornett
@@ -25,7 +18,6 @@
 #ifdef GDK_WINDOWING_X11
     #include <gdk/gdkx.h>
 #endif
-#include "wx/gtk/private/gtk2-compat.h"
 
 GtkWidget* wxGetRootWindow();
 
