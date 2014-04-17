e1ecc4cc3faef62b497f799fd3207623cec7e468
Paul Cornett <paulcor@bullseye.com>
2014-04-17 16:15:28 +0000

    allow using wxEventLoopGuarantor when wxUSE_GUI==1 fixes wxOSX-Cocoa build when wxUSE_CONSOLE_EVENTLOOP==0 closes #14980
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76364 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/evtloop.h
+++ include/wx/evtloop.h
@@ -427,7 +427,7 @@ private:
     wxEventLoopBase *m_evtLoopOld;
 };
 
-#if wxUSE_CONSOLE_EVENTLOOP
+#if wxUSE_GUI || wxUSE_CONSOLE_EVENTLOOP
 
 class wxEventLoopGuarantor
 {
@@ -455,6 +455,6 @@ private:
     wxEventLoop *m_evtLoopNew;
 };
 
-#endif // wxUSE_CONSOLE_EVENTLOOP
+#endif // wxUSE_GUI || wxUSE_CONSOLE_EVENTLOOP
 
 #endif // _WX_EVTLOOP_H_
