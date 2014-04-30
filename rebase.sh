59453669bdd232023c56d64f0d383a4d25a9b17a
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2014-04-30 04:19:29 +0200

    refined support script
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- rebase.sh
+++ rebase.sh
@@ -6,6 +6,12 @@ cd -
 git branch -D merge 2>/dev/null ; git fetch wx_git && git checkout -b merge && git rebase merge wx_git/master ; while true ; do ./rebase-second.sh ; git add . ; git rebase --continue || git rebase --skip ; test -d ".git/rebase-merge" -o -d ".git/rebase-apply" || break ; done
 git branch -D merge 2>/dev/null
 cp -rf ../wxWidgets_vanilla_trunk/* .
+cd ../wxWidgets_vanilla_trunk/
+dirs=`find . -mindepth 1 -type d | grep -v '^\./\.git/'`
+files=`find . -mindepth 1 -type f | grep -v '^\./\.git/'`
+cd -
+for i in ${dirs} ; do mkdir ${i} 2>/dev/null ; done
+for i in ${files} ; do cp -f "../wxWidgets_vanilla_trunk/${i}" "./${i}" ; done
 cd wxWidgets_vdm_patch && ./X64/Release/wxWidgets_vdm_patch.exe -p --no-wait && cd .. && rm -f build/msw/*.bcc build/msw/*.dsp build/msw/*.dsw build/msw/*.gcc build/msw/wx_vc10* build/msw/wx_vc7* build/msw/wx_vc8* build/msw/wx_vc9* build/msw/wx_vc10* build/msw/wx_vc11* build/msw/wx_vc12* build/msw/*.vc build/msw/*.wat build/msw/*.props
 git commit -a --signoff -m 'sync with upstream; patched for ICC; removed files we do not support'
 cp -rf ../build .
