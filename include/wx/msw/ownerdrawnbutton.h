fdd28fff4941ab48b689e5f162b6562c83a21539
Vadim Zeitlin <vadim@wxwidgets.org>
2014-05-07 13:10:17 +0000

    Fix harmless C4275 MSVC warning in wxMSW DLL builds.
    Since the addition of wxMSWOwnerDrawnButton, DLL builds started generated a
    lot of warnings about using this non DLL-exported class as base class for the
    DLL-exported wxCheckBox and wxRadioButton.
    
    Simply suppress these warnings as they are harmless in this case because the
    base class has no static data, which is the real problem this warning hints at.
    
    Closes #16237.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76478 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- include/wx/msw/ownerdrawnbutton.h
+++ include/wx/msw/ownerdrawnbutton.h
@@ -24,9 +24,11 @@
 // This class contains the type-independent part of wxMSWOwnerDrawnButton and
 // is implemented in src/msw/control.cpp.
 //
-// Notice that it is intentionally not exported, it is internal implementation
-// detail only.
-class WXDLLIMPEXP_CORE wxMSWOwnerDrawnButtonBase
+// Notice that it is intentionally not DLL-exported, it is internal
+// implementation detail only. However MSVS generates a warning C4275 when
+// deriving from the non-exported classes, so it needs to be explicitly
+// disabled when deriving from this one.
+class wxMSWOwnerDrawnButtonBase
 {
 protected:
     // Ctor takes the back pointer to the real window, must be non-NULL.
