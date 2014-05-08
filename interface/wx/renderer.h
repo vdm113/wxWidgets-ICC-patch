d411723da027bf899c1c7298c82172f9f3845d8f
Vadim Zeitlin <vadim@wxwidgets.org>
2014-05-08 14:43:27 +0000

    Fix harmless warning about using wxCONTROL_CHECKED in ternary operator.
    Add wxCONTROL_NONE which is just a symbolic name for 0 and use it in the code
    to avoid g++ 4.8 warnings about mixing enum and int in conditional expression.
    
    Closes #16242.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76484 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- interface/wx/renderer.h
+++ interface/wx/renderer.h
@@ -19,6 +19,13 @@
 */
 enum
 {
+    /**
+        Default state, no special flags.
+
+        @since 3.1.0
+     */
+    wxCONTROL_NONE       = 0x00000000,
+
     /** Control is disabled. */
     wxCONTROL_DISABLED   = 0x00000001,
 
