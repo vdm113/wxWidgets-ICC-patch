28aeecbc0bd8a2d5ef684996d0a52b81c04bb619
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2014-05-20 14:21:11 +0200

    remove files we do not support and refine sup[port script
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- rebase.sh
+++ rebase.sh
@@ -12,7 +12,8 @@ files=`find . -mindepth 1 -type f | grep -v '^\./\.git/'`
 cd -
 for i in ${dirs} ; do mkdir ${i} 2>/dev/null ; done
 for i in ${files} ; do cp -f "../wxWidgets_vanilla_trunk/${i}" "./${i}" ; done
-cd wxWidgets_vdm_patch && ./X64/Release/wxWidgets_vdm_patch.exe -p --no-wait && cd .. && rm -f build/msw/*.bcc build/msw/*.dsp build/msw/*.dsw build/msw/*.gcc build/msw/wx_vc10* build/msw/wx_vc7* build/msw/wx_vc8* build/msw/wx_vc9* build/msw/wx_vc10* build/msw/wx_vc11* build/msw/wx_vc12* build/msw/*.vc build/msw/*.wat build/msw/*.props
+cd wxWidgets_vdm_patch && ./X64/Release/wxWidgets_vdm_patch.exe -p --no-wait && cd ..
+for i in build/msw/*.bcc build/msw/*.dsp build/msw/*.dsw build/msw/*.gcc build/msw/wx_vc10* build/msw/wx_vc7* build/msw/wx_vc8* build/msw/wx_vc9* build/msw/wx_vc10* build/msw/wx_vc11* build/msw/wx_vc12* build/msw/*.vc build/msw/*.wat build/msw/*.props ; do ( git rm -f ${i} >/dev/null ) ; ( rm -f ${i} >/dev/null ) ; done
 git commit -a --signoff -m 'sync with upstream; patched for ICC; removed files we do not support'
 cp -rf ../build .
 git commit -a --signoff -m 'restore our project files'
