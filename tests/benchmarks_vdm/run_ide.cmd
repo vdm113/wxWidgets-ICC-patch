9ed2659943a21326cf1c035769e49450cf7d31dc
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2014-06-16 03:31:17 +0200

    no real changes, just remember command-line arguments into remark
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- tests/benchmarks_vdm/run_ide.cmd
+++ tests/benchmarks_vdm/run_ide.cmd
@@ -1,2 +1,4 @@
 set Path=..\..\lib\vc_x64_dll\;%Path%
 start bench_vc12.sln
+
+rem --num-runs 1000 ParseHTML CharBuffer MSWCompareStringIgnoreCase MSWCompareString MSWlstrcmpi MSWlstrcmp StringCmpNoCase StringCmp StricmpW StricmpA StrcmpW StrcmpA Upper Lower ReplaceShorter ReplaceLonger ReplaceAll ReplaceSome ReplaceNone ReplaceLoop ForStringRIter ForStringIter ForStringIndex ForCString FromAsciiWithLen FromAsciiWithNpos FromAscii FromUTF8UncheckedWithLen FromUTF8UncheckedWithNpos FromUTF8Unchecked FromUTF8WithLen FromUTF8WithNpos FromUTF8 Strlen ShrinkHighQuality ShrinkNormal EnlargeHighQuality EnlargeNormal LoadTIFF LoadPNG LoadJPEG LoadBMP ParseDate
