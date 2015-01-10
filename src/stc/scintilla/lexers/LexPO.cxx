2e03895f5e0edbc17e5aec7c2b17fe8d8d69e93a
ARATA Mizuki <minorinoki@gmail.com>
2015-01-10 20:07:55 +0900

    Update Scintilla from 3.4.1 to 3.4.2

--- src/stc/scintilla/lexers/LexPO.cxx
+++ src/stc/scintilla/lexers/LexPO.cxx
@@ -49,11 +49,6 @@ static void ColourisePODoc(unsigned int startPos, int length, int initStyle, Wor
 	// the line state holds the last state on or before the line that isn't the default style
 	int curLineState = curLine > 0 ? styler.GetLineState(curLine - 1) : SCE_PO_DEFAULT;
 	
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
 	for (; sc.More(); sc.Forward()) {
 		// whether we should leave a state
 		switch (sc.state) {
@@ -116,11 +111,6 @@ static void ColourisePODoc(unsigned int startPos, int length, int initStyle, Wor
 				if (curLineState == SCE_PO_COMMENT)
 					curLineState = SCE_PO_DEFAULT;
 				
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
 				while (sc.More() && ! sc.atLineEnd && isspacechar(sc.ch))
 					sc.Forward();
 			}
@@ -195,11 +185,6 @@ static void FoldPODoc(unsigned int startPos, int length, int, WordList *[], Acce
 	int visible = 0;
 	int chNext = styler[startPos];
 	
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
 	for (unsigned int i = startPos; i < endPos; i++) {
 		int ch = chNext;
 		chNext = styler.SafeGetCharAt(i+1);
