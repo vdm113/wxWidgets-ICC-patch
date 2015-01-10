5724c716c1710cb896fecca9006a082f6a2b1ef4
ARATA Mizuki <minorinoki@gmail.com>
2015-01-10 22:20:01 +0900

    Update Scintilla from 3.4.4 to 3.5.2

--- src/stc/scintilla/src/PerLine.cxx
+++ src/stc/scintilla/src/PerLine.cxx
@@ -558,11 +558,6 @@ LineTabstops::~LineTabstops() {
 }
 
 void LineTabstops::Init() {
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
 	for (int line = 0; line < tabstops.Length(); line++) {
 		delete tabstops[line];
 	}
@@ -617,11 +612,6 @@ int LineTabstops::GetNextTabstop(int line, int x) const {
 	if (line < tabstops.Length()) {
 		TabstopList *tl = tabstops[line];
 		if (tl) {
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
 			for (size_t i = 0; i < tl->size(); i++) {
 				if ((*tl)[i] > x) {
 					return (*tl)[i];
