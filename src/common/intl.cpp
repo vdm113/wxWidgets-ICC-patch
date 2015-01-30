60e6f40969b5664954d3036c0705c8261346eb0f
Václav Slavík <vslavik@fastmail.fm>
2015-01-30 15:53:02 +0000

    Handle 5+ letter codes in TranslateFromUnicodeFormat()
    Unicode TR #35 v26 allows for five-letter (MMMMM) or even six-letter
    (EEEEEE) forms of some of the fields, but TranslateFromUnicodeFormat()
    asserts in these situations.
    
    Fix it to fall back to short forms for MMMMM and EEEEEE that are used in
    practice e.g. on OS X if the user has custom formatting settings.
    
    Consulting the table of sensible specifiers from
    http://userguide.icu-project.org/formatparse/datetime, it appears these
    two were the only omissions in this function.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@78422 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/common/intl.cpp
+++ src/common/intl.cpp
@@ -1300,6 +1300,8 @@ static wxString TranslateFromUnicodeFormat(const wxString& fmt)
                             fmtWX += "%A";
                             break;
                         case 5: // EEEEE
+                        case 6: // EEEEEE
+                            // no "narrow form" in strftime(), use abbrev.
                             fmtWX += "%a";
                             break;
 
@@ -1323,6 +1325,11 @@ static wxString TranslateFromUnicodeFormat(const wxString& fmt)
 
                         case 4:
                             fmtWX += "%B";
+                            break;
+
+                        case 5:
+                            // no "narrow form" in strftime(), use abbrev.
+                            fmtWX += "%b";
                             break;
 
                         default:
