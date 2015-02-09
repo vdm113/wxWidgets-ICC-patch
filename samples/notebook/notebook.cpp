fda8dbe631f9c9e12c3cced3224fd127ff823acd
Vadim Zeitlin <vadim@wxwidgets.org>
2015-02-09 00:26:11 +0000

    Add wxAddRemoveCtrl class.
    This is a simple high level helper combining an arbitrary control showing
    multiple items with the buttons allowing to add items to and remove items from
    this control, but using the buttons and the layout appropriate for the current
    platform.
    
    Add the implementation itself, an example of using it to the dialogs sample
    and the documentation.
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@78462 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/notebook/notebook.cpp
+++ samples/notebook/notebook.cpp
@@ -896,6 +896,7 @@ void MyFrame::OnDeleteLastPage(wxCommandEvent& WXUNUSED(event))
 
 void MyFrame::OnNextPage(wxCommandEvent& WXUNUSED(event))
 {
+    wxFindWindowAtPoint(wxPoint(0,0));
     wxBookCtrlBase *currBook = GetCurrentBook();
 
     if ( currBook )
