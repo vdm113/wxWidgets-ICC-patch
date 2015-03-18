5fb7609cdfb42f32f022693cad795a9a4f0f59a0
Vadim Zeitlin <vadim@wxwidgets.org>
2015-03-18 16:10:31 +0100

    Add XRC handler for wxActivityIndicator control.
    The only attribute for this control is a boolean "running" which can be set to
    start the indicator on load.
    
    Update the schema, documentation and the XRC sample.

--- src/xrc/xh_activityindicator.cpp
+++ src/xrc/xh_activityindicator.cpp
@@ -1,12 +1,3 @@
-/* token_VDM_prologue */
-#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(VDM_MACRO_PRAGMA_IVDEP)
-#   define VDM_MACRO_PRAGMA_IVDEP __pragma(ivdep) __pragma(swp) __pragma(unroll) __pragma(prefetch)
-#   define VDM_MACRO_PRAGMA_NO_IVDEP /* NOP */
-#elif !defined(VDM_MACRO_PRAGMA_IVDEP)
-#   define VDM_MACRO_PRAGMA_IVDEP /* NOP */
-#   define VDM_MACRO_PRAGMA_NO_IVDEP /* NOP */
-#endif
-
 /////////////////////////////////////////////////////////////////////////////
 // Name:        src/xrc/xh_activityindicator.cpp
 // Purpose:     Implementation of wxActivityIndicator XRC handler.
@@ -28,7 +19,7 @@
 #include "wx/xrc/xh_activityindicator.h"
 #include "wx/activityindicator.h"
 
-wxIMPLEMENT_DYNAMIC_CLASS(wxActivityIndicatorXmlHandler, wxXmlResourceHandler);
+IMPLEMENT_DYNAMIC_CLASS(wxActivityIndicatorXmlHandler, wxXmlResourceHandler)
 
 wxActivityIndicatorXmlHandler::wxActivityIndicatorXmlHandler()
 {
