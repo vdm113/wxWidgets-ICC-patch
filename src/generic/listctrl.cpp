0fade967250a3ccb6a9b4e7354e6d498cc8e06f2
Vadim Zeitlin <vadim@wxwidgets.org>
2015-03-19 16:38:36 +0100

    Fix wxGenericListCtrl autosizing after deleting all its items.
    Set bNeedsUpdate flag for all columns even when the control is already empty,
    this ensures that things will work if more items are added to it later.
    
    See #10326.

--- src/generic/listctrl.cpp
+++ src/generic/listctrl.cpp
@@ -4240,11 +4240,6 @@ void wxListMainWindow::DoDeleteAllItems()
     // We will need to update all columns if any items are inserted again.
     if ( InReportView() )
     {
-#if defined(__INTEL_COMPILER) && 1 // VDM auto patch
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif
         for ( size_t i = 0; i < m_aColWidths.GetCount(); i++ )
             m_aColWidths.Item(i)->bNeedsUpdate = true;
     }
