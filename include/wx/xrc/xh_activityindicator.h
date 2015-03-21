c8535805c4d75b5645faa4697393d887573ba0ac
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2015-03-21 16:40:14 +0100

    upstreaa have addeded the files
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

---
+++ include/wx/xrc/xh_activityindicator.h
@@ -0,0 +1,38 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(VDM_MACRO_PRAGMA_IVDEP)
+#   define VDM_MACRO_PRAGMA_IVDEP __pragma(ivdep) __pragma(swp) __pragma(unroll)
+#elif !defined(VDM_MACRO_PRAGMA_IVDEP)
+#   define VDM_MACRO_PRAGMA_IVDEP
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wx/xrc/xh_activityindicator.h
+// Purpose:     Declaration of wxActivityIndicator XRC handler.
+// Author:      Vadim Zeitlin
+// Created:     2015-03-18
+// Copyright:   (c) 2015 Vadim Zeitlin <vadim@wxwidgets.org>
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WX_XH_ACTIVITYINDICATOR_H_
+#define _WX_XH_ACTIVITYINDICATOR_H_
+
+#include "wx/xrc/xmlres.h"
+
+#if wxUSE_XRC && wxUSE_ACTIVITYINDICATOR
+
+class WXDLLIMPEXP_XRC wxActivityIndicatorXmlHandler : public wxXmlResourceHandler
+{
+public:
+    wxActivityIndicatorXmlHandler();
+
+    virtual wxObject *DoCreateResource() wxOVERRIDE;
+    virtual bool CanHandle(wxXmlNode *node) wxOVERRIDE;
+
+private:
+    wxDECLARE_DYNAMIC_CLASS(wxActivityIndicatorXmlHandler);
+};
+
+#endif // wxUSE_XRC && wxUSE_ACTIVITYINDICATOR
+
+#endif // _WX_XH_ACTIVITYINDICATOR_H_
---
+++ src/xrc/xh_activityindicator.cpp
@@ -0,0 +1,59 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(VDM_MACRO_PRAGMA_IVDEP)
+#   define VDM_MACRO_PRAGMA_IVDEP __pragma(ivdep) __pragma(swp) __pragma(unroll)
+#elif !defined(VDM_MACRO_PRAGMA_IVDEP)
+#   define VDM_MACRO_PRAGMA_IVDEP
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// Name:        src/xrc/xh_activityindicator.cpp
+// Purpose:     Implementation of wxActivityIndicator XRC handler.
+// Author:      Vadim Zeitlin
+// Created:     2015-03-18
+// Copyright:   (c) 2015 Vadim Zeitlin <vadim@wxwidgets.org>
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+// For compilers that support precompilation, includes "wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#if wxUSE_XRC && wxUSE_ACTIVITYINDICATOR
+
+#include "wx/xrc/xh_activityindicator.h"
+#include "wx/activityindicator.h"
+
+IMPLEMENT_DYNAMIC_CLASS(wxActivityIndicatorXmlHandler, wxXmlResourceHandler)
+
+wxActivityIndicatorXmlHandler::wxActivityIndicatorXmlHandler()
+{
+    AddWindowStyles();
+}
+
+wxObject *wxActivityIndicatorXmlHandler::DoCreateResource()
+{
+    XRC_MAKE_INSTANCE(ctrl, wxActivityIndicator)
+
+    ctrl->Create(m_parentAsWindow,
+                 GetID(),
+                 GetPosition(), GetSize(),
+                 GetStyle(),
+                 GetName());
+
+    SetupWindow(ctrl);
+
+    if ( GetBool(wxS("running")) )
+        ctrl->Start();
+
+    return ctrl;
+}
+
+bool wxActivityIndicatorXmlHandler::CanHandle(wxXmlNode *node)
+{
+    return IsOfClass(node, wxS("wxActivityIndicator"));
+}
+
+#endif // wxUSE_XRC && wxUSE_ACTIVITYINDICATOR
