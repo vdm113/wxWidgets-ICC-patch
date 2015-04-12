6d5220a86eaba13eb0317f15e2e730f6807bf84f
Vadim Zeitlin <vadim@wxwidgets.org>
2015-04-12 18:21:47 +0200

    Restore OnRun() in the non-GUI unit test suite.
    Commit 3e67b1ef6801f146c0d7463cab316bf8053469f2 broke the test suite in
    non-GUI case, we still need to override OnRun() then to run the tests.
    
    Also make it more clear that m_exitcode is only used in the GUI case.

--- tests/test.cpp
+++ tests/test.cpp
@@ -351,7 +351,12 @@ public:
 
         return m_exitcode;
     }
-#endif // wxUSE_GUI
+#else // !wxUSE_GUI
+    virtual int OnRun()
+    {
+        return RunTests();
+    }
+#endif // wxUSE_GUI/!wxUSE_GUI
 
 private:
     void List(Test *test, const string& parent = "") const;
@@ -382,8 +387,10 @@ private:
     FilterEventFunc m_filterEventFunc;
     ProcessEventFunc m_processEventFunc;
 
+#if wxUSE_GUI
     // the program exit code
     int m_exitcode;
+#endif // wxUSE_GUI
 };
 
 IMPLEMENT_APP_NO_MAIN(TestApp)
@@ -519,7 +526,9 @@ TestApp::TestApp()
 
     m_locale = NULL;
 
+#if wxUSE_GUI
     m_exitcode = EXIT_SUCCESS;
+#endif // wxUSE_GUI
 }
 
 // Init
