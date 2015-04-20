a6e0b2799c02504003a5c519b22eb5b7bc4bc253
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2015-04-20 19:33:20 +0200

    sync with upstream (GIT); patched for ICC; removed files we do not support
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/common/addremovectrl.cpp
+++ src/common/addremovectrl.cpp
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(VDM_MACRO_PRAGMA_IVDEP)
+#   define VDM_MACRO_PRAGMA_IVDEP __pragma(ivdep) __pragma(swp) __pragma(unroll)
+#elif !defined(VDM_MACRO_PRAGMA_IVDEP)
+#   define VDM_MACRO_PRAGMA_IVDEP
+#endif
+
 ///////////////////////////////////////////////////////////////////////////////
 // Name:        src/common/addremovectrl.cpp
 // Purpose:     wxAddRemoveCtrl implementation.
@@ -40,7 +47,7 @@
 // ----------------------------------------------------------------------------
 
 extern
-WXDLLIMPEXP_DATA_CORE(const char) wxAddRemoveCtrlNameStr[] = "wxAddRemoveCtrl";
+WXDLLIMPEXP_DATA_ADV(const char) wxAddRemoveCtrlNameStr[] = "wxAddRemoveCtrl";
 
 bool
 wxAddRemoveCtrl::Create(wxWindow* parent,
