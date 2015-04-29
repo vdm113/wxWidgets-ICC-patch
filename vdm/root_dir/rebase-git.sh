415028b656bcab0144f3fae5831371d834fc11a4
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2015-04-29 13:57:46 +0200

    refine our support script regarding of deletion of old MMMSVC project files
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- vdm/root_dir/rebase-git.sh
+++ vdm/root_dir/rebase-git.sh
@@ -20,10 +20,13 @@ for i in ${dirs} ; do mkdir ${i} 2>/dev/null ; done
 for i in ${files} ; do cp -f "../wxWidgets_vanilla_trunk/${i}" "./${i}" ; done
 cd wxWidgets_vdm_patch && ./X64/Release/wxWidgets_vdm_patch.exe -p --no-wait && cd ..
 for i in `find build/msw/ -type f -maxdepth 1` ; do ( git rm -f "${i}" 2>/dev/null ) ; ( rm -f "${i}" 2>/dev/null ) ; done
-git commit -a --signoff -m 'sync with upstream (GIT); patched for ICC; removed files we do not support'
+git commit -a --signoff -m 'sync with upstream (GIT); patched for ICC'
 for i in `find build/msw/ -type f -maxdepth 1` ; do ( git rm -f "${i}" 2>/dev/null ) ; ( rm -f "${i}" 2>/dev/null ) ; done
-git add build/msw/
 git commit -a --signoff -m 'restore our project files'
+git reset --hard
+git add build/msw/
+git clean -f -d
+gin commit -a --signoff -m 'remove obsolete project files we do not support'
 mv -f README.md README_orig_wx.md
 cp -f ./vdm/root_dir/* ./vdm/root_dir/.??* .
 git add README.md
