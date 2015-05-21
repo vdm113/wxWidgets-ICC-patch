8639bbffd1dc008566dfa25fa758edc8c16b24c7
Artur Wieczorek <artwik@wp.pl>
2015-05-21 18:51:08 +0200

    Allow wxPG to take focus on the entire area even if wxPG is not a standalone control.
    By default, to prevent wxPG from stealing focus from other controls, focus is moved to the grid only if it was already in one of its child controls.
    When newly introduced wxPG_EX_ALWAYS_ALLOW_FOCUS flag is set then wxPG can take focus on the entire grid area (on canvas) even if focus is moved from another control.
    Default wxPG behavior remains unchanged because wxPG_EX_ALWAYS_ALLOW_FOCUS flag must be explicitly set with wxPropertyGrid::SetExtraStyle function.
    
    Closes #16993.

--- src/propgrid/propgrid.cpp
+++ src/propgrid/propgrid.cpp
@@ -4872,11 +4872,6 @@ void wxPropertyGrid::SetFocusOnCanvas()
         if ( focus )
         {
             wxWindow* parent = focus->GetParent();
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
             while ( parent )
             {
                 if ( parent == this )
