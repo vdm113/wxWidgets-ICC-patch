1a02cc2c7dfe40956fe86e0a163b538e0c6f645a
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2015-05-25 03:06:13 +0200

    sync with upstream (GIT); patched for ICC
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/generic/activityindicator.cpp
+++ src/generic/activityindicator.cpp
@@ -1,3 +1,10 @@
+/* token_VDM_prologue */
+#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(VDM_MACRO_PRAGMA_IVDEP)
+#   define VDM_MACRO_PRAGMA_IVDEP __pragma(ivdep) __pragma(swp) __pragma(unroll)
+#elif !defined(VDM_MACRO_PRAGMA_IVDEP)
+#   define VDM_MACRO_PRAGMA_IVDEP
+#endif
+
 ///////////////////////////////////////////////////////////////////////////////
 // Name:        src/generic/activityindicator.cpp
 // Purpose:     Generic wxActivityIndicator implementation.
@@ -24,11 +31,12 @@
 
 #if wxUSE_ACTIVITYINDICATOR && !defined(__WXGTK3__)
 
+#include "wx/activityindicator.h"
+
 #ifndef WX_PRECOMP
+    #include "wx/dcclient.h"
     #include "wx/timer.h"
 #endif // WX_PRECOMP
-
-#include "wx/activityindicator.h"
 
 #include "wx/graphics.h"
 #include "wx/scopedptr.h"
@@ -149,7 +157,12 @@ private:
         // the next position every time.
         gc->Rotate(m_frame*angle);
 
-        const bool isEnabled = m_win->IsEnabled();
+        const bool isEnabled = m_win->IsThisEnabled();
+#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
+#   pragma ivdep
+#   pragma swp
+#   pragma unroll
+#endif /* VDM auto patch */
         for ( int n = 0; n < NUM_DOTS; n++ )
         {
             // Draw all dots uniformly grey when the window is disabled,
