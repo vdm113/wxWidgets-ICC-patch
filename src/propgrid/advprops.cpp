c67503beb8367a4a617cec05da0697452c789cba
Artur Wieczorek <artwik@wp.pl>
2015-05-31 19:12:29 +0200

    Replace wxT() with wxS() in wxPropertyGrid code.

--- src/propgrid/advprops.cpp
+++ src/propgrid/advprops.cpp
@@ -2078,16 +2078,7 @@ void wxMultiChoiceProperty::GenerateValueAsString( wxVariant& value,
     if ( itemCount )
         tempStr.append( wxS("\"") );
 
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#   pragma prefetch
-#   if 0
-#       pragma simd noassert
-#   endif
-#endif /* VDM auto patch */
-    for ( size_t i = 0; i < itemCount; i++ )
+    for ( i = 0; i < itemCount; i++ )
     {
         tempStr.append( strings[i] );
         tempStr.append( wxS("\"") );
