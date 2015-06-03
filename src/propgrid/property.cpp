99fbac91de5503e92c46235e91ee9e4e4534d073
Jan van Dijk <jan@epgmod.phys.tue.nl>
2015-06-03 17:08:30 +0200

    Don't test if a reference is null, this cannot happen.
    Testing for null references is unnecessary and just results in clang warnings.
    
    Closes #17016.

--- src/propgrid/property.cpp
+++ src/propgrid/property.cpp
@@ -3356,7 +3356,7 @@ wxArrayString wxPGChoices::GetLabels() const
 {
     wxArrayString arr;
 
-    if ( this && IsOk() )
+    if ( IsOk() )
         for ( unsigned int i = 0; i < GetCount(); i++ )
             arr.push_back(GetLabel(i));
 
