731999a2ae641f5df8efa927a73cf0d57fca97aa
Dimitri Schoolwerth <dimitri@schoolwerth.com>
2015-06-05 02:54:02 +0400

    Fix some typos, no code changes (besides strings)

--- src/gtk/webview_webkit.cpp
+++ src/gtk/webview_webkit.cpp
@@ -147,15 +147,6 @@ wxgtk_webview_webkit_navigation(WebKitWebView *,
         wxSharedPtr<wxWebViewHandler> handler;
         wxVector<wxSharedPtr<wxWebViewHandler> > handlers = webKitCtrl->GetHandlers();
         //We are not vetoed so see if we match one of the additional handlers
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#   pragma prefetch
-#   if 0
-#       pragma simd noassert
-#   endif
-#endif /* VDM auto patch */
         for(wxVector<wxSharedPtr<wxWebViewHandler> >::iterator it = handlers.begin();
             it != handlers.end(); ++it)
         {
@@ -378,15 +369,6 @@ wxgtk_webview_webkit_resource_req(WebKitWebView *,
     wxVector<wxSharedPtr<wxWebViewHandler> > handlers = webKitCtrl->GetHandlers();
 
     //We are not vetoed so see if we match one of the additional handlers
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#   pragma prefetch
-#   if 0
-#       pragma simd noassert
-#   endif
-#endif /* VDM auto patch */
     for(wxVector<wxSharedPtr<wxWebViewHandler> >::iterator it = handlers.begin();
         it != handlers.end(); ++it)
     {
