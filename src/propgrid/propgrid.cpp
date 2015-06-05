8415506b0e6c94d49cf045b22fdfb7dc3b21b0ef
Artur Wieczorek <artwik@wp.pl>
2015-06-05 22:47:28 +0200

    Refactor string manipulations in wxPropertyGrid::ExpandEscapeSequence and CreateEscapeSequences.
    Share wxString::clear() method to initialize empty destination string.

--- src/propgrid/propgrid.cpp
+++ src/propgrid/propgrid.cpp
@@ -1801,11 +1801,6 @@ wxString& wxPropertyGrid::ExpandEscapeSequences( wxString& dst_str, wxString& sr
 
     wxString::const_iterator i = src_str.begin();
 
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
     for ( ; i != src_str.end(); ++i )
     {
         wxUniChar a = *i;
@@ -1863,12 +1858,7 @@ wxString& wxPropertyGrid::CreateEscapeSequences( wxString& dst_str, wxString& sr
     wxString::const_iterator i;
     wxUniChar prev_a = wxS('\0');
 
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
-    for ( i = src_str.begin(); i != src_str.end(); ++i )
+    for ( ; i != src_str.end(); ++i )
     {
         wxUniChar a = *i;
 
