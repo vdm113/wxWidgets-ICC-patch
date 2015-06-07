47ab9a78ac5e35df5bf69149471d80412289c674
Marian 'VooDooMan' Meravy <vdm113@gmail.com>
2015-06-08 01:51:30 +0200

    sync with upstream (GIT); patched for ICC
    Signed-off-by: Marian 'VooDooMan' Meravy <vdm113@gmail.com>

--- src/propgrid/props.cpp
+++ src/propgrid/props.cpp
@@ -900,6 +900,11 @@ const wxString& wxPropertyGrid::DoubleToString(wxString& target,
         bool isZero = true;
         wxString::const_iterator i = target.begin() + 1;
 
+#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
+#   pragma ivdep
+#   pragma swp
+#   pragma unroll
+#endif /* VDM auto patch */
         for ( ; i != target.end(); ++i )
         {
             if ( *i != wxS('0') && *i != wxS('.') && *i != wxS(',') )
@@ -1702,6 +1707,11 @@ void wxFlagsProperty::OnSetValue()
     {
         // Set child modified states
         const wxPGChoices& choices = m_choices;
+#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
+#   pragma ivdep
+#   pragma swp
+#   pragma unroll
+#endif /* VDM auto patch */
         for ( unsigned int i = 0; i < GetItemCount(); i++ )
         {
             int flag;
