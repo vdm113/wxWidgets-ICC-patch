93c99b824ad85d12582da30fb15491bae2be7aed
Artur Wieczorek <artwik@wp.pl>
2015-06-09 20:04:27 +0200

    Fixed minor typos in comments.
    Yet another iteration.

--- include/wx/propgrid/manager.h
+++ include/wx/propgrid/manager.h
@@ -520,7 +520,7 @@ public:
     /** Select and displays a given page.
 
         @param index
-            Index of page being seleced. Can be -1 to select nothing.
+            Index of page being selected. Can be -1 to select nothing.
     */
     void SelectPage( int index );
 
@@ -630,7 +630,7 @@ protected:
     /**
         Creates property grid for the manager. Reimplement in derived class to
         use subclassed wxPropertyGrid. However, if you do this then you
-        must also use the two-step construction (ie. default constructor and
+        must also use the two-step construction (i.e. default constructor and
         Create() instead of constructor with arguments) when creating the
         manager.
     */
