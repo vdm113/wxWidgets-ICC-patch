3ee88f9b0b5be9a65f51a2247bb5d78495b0a26d
Vadim Zeitlin <vadim@wxwidgets.org>
2015-07-05 16:18:58 +0200

    Use blocking sockets from non-main threads in wxProtocol.
    Non-blocking sockets can't work in worker threads without additional locking
    as they generate events that can be dispatched from the main thread after the
    socket object, created in the worker thread, is already destroyed, so don't
    even attempt to use them if wxProtocol object is created from non-main thread.
    
    Also simplify the code by removing the calls to SetFlags(), Notify() and
    {Save,Restore}State() and simply put the socket from the beginning in
    blocking, wait all mode that it needs to be in.
    
    This, with the fixes in the previous commit, allows wxHTTP and wxFTP to work
    from worker threads too.
    
    Test using wxHTTP from a worker thread in the socket client sample.
    
    Closes #17031.

--- src/common/ftp.cpp
+++ src/common/ftp.cpp
@@ -86,8 +86,6 @@ wxFTP::wxFTP()
     m_username = wxT("anonymous");
     m_password << wxGetUserId() << wxT('@') << wxGetFullHostName();
 
-    SetNotify(0);
-    SetFlags(wxSOCKET_NOWAIT);
     m_bPassive = true;
     m_bEncounteredError = false;
 }
@@ -798,8 +796,6 @@ wxInputStream *wxFTP::GetInputStream(const wxString& path)
         m_lastError = wxPROTO_CONNERR;
         return NULL;
     }
-
-    sock->SetFlags(wxSOCKET_WAITALL);
 
     m_streaming = true;
 
