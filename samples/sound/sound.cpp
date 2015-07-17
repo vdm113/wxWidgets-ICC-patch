43cba190e37ec6c565d0f9d276988ec68178af2d
Vadim Zeitlin <vadim@zeitlins.org>
2015-07-18 00:32:56 +0200

    Don't keep using invalid wxSound object in the sound sample.
    If creating a sound object fails, delete it to ensure that it is recreated
    later.
    
    This fixes a minor bug: previously, if an invalid file was used as sound file,
    only the first attempt to play it resulted in an error and all the subsequent
    ones were just silently ignored. Now every attempt to play an invalid file
    results in an error message, as expected.

--- samples/sound/sound.cpp
+++ samples/sound/sound.cpp
@@ -88,6 +88,7 @@ public:
 
 private:
     bool CreateSound(wxSound& snd) const;
+    wxSound* TryCreateSound() const;
 
     wxSound*    m_sound;
     wxString    m_soundFile;
@@ -982,6 +983,17 @@ bool MyFrame::CreateSound(wxSound& snd) const
     return snd.Create(m_soundFile);
 }
 
+wxSound* MyFrame::TryCreateSound() const
+{
+    wxSound* const sound = new wxSound;
+    if ( !CreateSound(*sound) )
+    {
+        delete sound;
+        return NULL;
+    }
+
+    return sound;
+}
 
 void MyFrame::NotifyUsingFile(const wxString& name)
 {
@@ -1061,12 +1073,9 @@ void MyFrame::OnPlaySync(wxCommandEvent& WXUNUSED(event))
 {
     wxBusyCursor busy;
     if ( !m_sound )
-    {
-        m_sound = new wxSound;
-        CreateSound(*m_sound);
-    }
+        m_sound = TryCreateSound();
 
-    if (m_sound->IsOk())
+    if (m_sound)
         m_sound->Play(wxSOUND_SYNC);
 }
 
@@ -1074,12 +1083,9 @@ void MyFrame::OnPlayAsync(wxCommandEvent& WXUNUSED(event))
 {
     wxBusyCursor busy;
     if ( !m_sound )
-    {
-        m_sound = new wxSound;
-        CreateSound(*m_sound);
-    }
+        m_sound = TryCreateSound();
 
-    if (m_sound->IsOk())
+    if (m_sound)
         m_sound->Play(wxSOUND_ASYNC);
 }
 
@@ -1096,10 +1102,7 @@ void MyFrame::OnPlayLoop(wxCommandEvent& WXUNUSED(event))
 {
     wxBusyCursor busy;
     if ( !m_sound )
-    {
-        m_sound = new wxSound;
-        CreateSound(*m_sound);
-    }
+        m_sound = TryCreateSound();
 
     if (m_sound->IsOk())
         m_sound->Play(wxSOUND_ASYNC | wxSOUND_LOOP);
