eb4a47cb0a23368d325a68155bd41c11d5438bd8
Vadim Zeitlin <vadim@wxwidgets.org>
2015-07-27 03:28:07 +0200

    Fix warnings about pointer/int casts in Win32 part of libtiff too.
    Do the same thing for tif_win32.c as f995dfcc20b7e930029544dc70a7780509b8c028
    did for tif_unix.c, i.e. use a union for casting between HANDLEs and ints to
    avoid compiler warnings which were given for the explicit casts before.

--- src/tiff/libtiff/tif_win32.c
+++ src/tiff/libtiff/tif_win32.c
@@ -227,11 +227,8 @@ TIFFFdOpen(int ifd, const char* name, const char* mode)
 	int fSuppressMap;
 	int m;
 	fSuppressMap=0;
-#if defined(__INTEL_COMPILER) && 1 /* VDM auto patch */
-#   pragma ivdep
-#   pragma swp
-#   pragma unroll
-#endif /* VDM auto patch */
+	fd_as_handle_union_t fdh;
+	fdh.fd = ifd;
 	for (m=0; mode[m]!=0; m++)
 	{
 		if (mode[m]=='u')
@@ -240,7 +237,7 @@ TIFFFdOpen(int ifd, const char* name, const char* mode)
 			break;
 		}
 	}
-	tif = TIFFClientOpen(name, mode, (thandle_t)ifd,
+	tif = TIFFClientOpen(name, mode, fdh.h,
 			_tiffReadProc, _tiffWriteProc,
 			_tiffSeekProc, _tiffCloseProc, _tiffSizeProc,
 			fSuppressMap ? _tiffDummyMapProc : _tiffMapProc,
@@ -259,7 +256,7 @@ TIFF*
 TIFFOpen(const char* name, const char* mode)
 {
 	static const char module[] = "TIFFOpen";
-	thandle_t fd;
+	fd_as_handle_union_t fdh;
 	int m;
 	DWORD dwMode;
 	TIFF* tif;
@@ -275,19 +272,19 @@ TIFFOpen(const char* name, const char* mode)
 		default:			return ((TIFF*)0);
 	}
 
-	fd = (thandle_t)CreateFileA(name,
+	fdh.h = CreateFileA(name,
 		(m == O_RDONLY)?GENERIC_READ:(GENERIC_READ | GENERIC_WRITE),
 		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, dwMode,
 		(m == O_RDONLY)?FILE_ATTRIBUTE_READONLY:FILE_ATTRIBUTE_NORMAL,
 		NULL);
-	if (fd == INVALID_HANDLE_VALUE) {
+	if (fdh.h == INVALID_HANDLE_VALUE) {
 		TIFFErrorExt(0, module, "%s: Cannot open", name);
 		return ((TIFF *)0);
 	}
 
-	tif = TIFFFdOpen((int)fd, name, mode);
+	tif = TIFFFdOpen(fdh.fd, name, mode);
 	if(!tif)
-		CloseHandle(fd);
+		CloseHandle(fdh.h);
 	return tif;
 }
 
@@ -298,7 +295,7 @@ TIFF*
 TIFFOpenW(const wchar_t* name, const char* mode)
 {
 	static const char module[] = "TIFFOpenW";
-	thandle_t fd;
+	fd_as_handle_union_t fdh;
 	int m;
 	DWORD dwMode;
 	int mbsize;
@@ -316,12 +313,12 @@ TIFFOpenW(const wchar_t* name, const char* mode)
 		default:			return ((TIFF*)0);
 	}
 
-	fd = (thandle_t)CreateFileW(name,
+	fdh.h = CreateFileW(name,
 		(m == O_RDONLY)?GENERIC_READ:(GENERIC_READ|GENERIC_WRITE),
 		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, dwMode,
 		(m == O_RDONLY)?FILE_ATTRIBUTE_READONLY:FILE_ATTRIBUTE_NORMAL,
 		NULL);
-	if (fd == INVALID_HANDLE_VALUE) {
+	if (fdh.h == INVALID_HANDLE_VALUE) {
 		TIFFErrorExt(0, module, "%S: Cannot open", name);
 		return ((TIFF *)0);
 	}
@@ -340,10 +337,10 @@ TIFFOpenW(const wchar_t* name, const char* mode)
 				    NULL, NULL);
 	}
 
-	tif = TIFFFdOpen((int)fd,
+	tif = TIFFFdOpen(fdh.fd,
 			 (mbname != NULL) ? mbname : "<unknown>", mode);
 	if(!tif)
-		CloseHandle(fd);
+		CloseHandle(fdh.h);
 
 	_TIFFfree(mbname);
 
