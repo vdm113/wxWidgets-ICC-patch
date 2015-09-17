881721696ac264beb9495840e3e27dc9faf2a460
Tobias Taschner <TcT2k@users.noreply.github.com>
2015-09-17 14:46:27 +0200

    Add wxSystemThemedControl and use it in wxMSW
    wxSystemThemedControl allows to use the "system theme" (i.e. the theme used by
    the system applications such as file manager and which can, surprisingly, be
    different from the default one). Currently it is only implemented for wxMSW
    and does nothing under the other platforms.
    
    Use wxSystemThemedControl for wxDataViewCtrl, wxListCtrl and, optionally, if
    wxTR_TWIST_BUTTONS style is specified, wxTreeCtrl to give them more native
    appearance under MSW.
    
    Closes #16414.

--- include/wx/systhemectrl.h
+++ include/wx/systhemectrl.h
@@ -1,12 +1,3 @@
-/* token_VDM_prologue */
-#if defined(__INTEL_COMPILER) && defined(_MSC_VER) && !defined(VDM_MACRO_PRAGMA_IVDEP)
-#   define VDM_MACRO_PRAGMA_IVDEP __pragma(ivdep) __pragma(swp) __pragma(unroll) __pragma(prefetch)
-#   define VDM_MACRO_PRAGMA_NO_IVDEP /* NOP */
-#elif !defined(VDM_MACRO_PRAGMA_IVDEP)
-#   define VDM_MACRO_PRAGMA_IVDEP /* NOP */
-#   define VDM_MACRO_PRAGMA_NO_IVDEP /* NOP */
-#endif
-
 /////////////////////////////////////////////////////////////////////////////
 // Name:        wx/systhemectrl.h
 // Purpose:     Class to make controls appear in the systems theme
