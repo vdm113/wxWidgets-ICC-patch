78e6537790cdb16529eec7d970d17ceaf07ee322
Tobias Taschner <TcT2k@users.noreply.github.com>
2015-09-30 19:15:25 +0200

    Fix right aligned text position in wxDVC on MSW with system theme
    Right aligned text was one pixel off when inside selection rect.
    
    Close #16414

--- src/msw/renderer.cpp
+++ src/msw/renderer.cpp
@@ -1030,7 +1030,10 @@ void wxRendererXP::DrawItemText(wxWindow* win,
         if ( align & wxALIGN_CENTER_HORIZONTAL )
             textFlags |= DT_CENTER;
         else if ( align & wxALIGN_RIGHT )
+        {
             textFlags |= DT_RIGHT;
+            rc.right--; // Alignment is inconsistent with DrawLabel otherwise
+        }
         else
             textFlags |= DT_LEFT;
 
